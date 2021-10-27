6954f3af31c41f8bf5c1d663b69cdfbc4d90863a
shinley <chenxl4@autoai.com>
2021-10-27 13:48:59 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1专栏， 有一段非常有意思的简介, 可以更新一下',
+    avatar: require('@/assets/logo.png'),
   },
-});
+  {
+    id: 2,
+    title: 'test1的专栏',
+    description: '这是test1专栏， 有一段非常有意思的简介, 可以更新一下',
+    // avatar: require('./assets/logo.png'),
+  },
+  {
+    id: 3,
+    title: 'test1的专栏',
+    description: '这是test1专栏， 有一段非常有意思的简介, 可以更新一下',
+    avatar: require('./assets/logo.png'),
+  },
+  {
+    id: 4,
+    title: 'test1的专栏',
+    description: '这是test1专栏， 有一段非常有意思的简介, 可以更新一下',
+    avatar: require('./assets/logo.png'),
+  },
+]
+
+const currentUser: UserProps = {
+  id: 1,
+  name: 'shinley',
+  isLogin: false,
+}
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList,
+    GlobalHeader,
+  },
+  setup() {
+    return {
+      list: testData,
+      currentUser,
+    }
+  },
+})
 </script>
 
 <style>
